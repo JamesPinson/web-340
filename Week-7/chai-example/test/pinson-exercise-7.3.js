1ac2ff84b674479a309cf5863ef7ffc9321eeda3
James <jpinson@my365.bellevue.edu>
2021-02-18 21:31:57 -0600

    fixed.

--- Week-7/chai-example/test/pinson-exercise-7.3.js
+++ Week-7/chai-example/test/pinson-exercise-7.3.js
@@ -1,9 +1,19 @@
+/*
+============================================
+; Title: Pinson-Exercise 7.3.js
+; Author: Richard Krasso
+; Date: February 18th 2021
+; Modified By: James Pinson
+; Description: This assignment teaches us how to use Mocha and Chai.    
+;============================================
+*/
 var fruits = require("../Pinson-fruits");
 
 var chai = require("chai");
 
 var assert = chai.assert;
 
+//This sets up what Chai expects the function to return. 
 describe("fruits", function(){
     it("should return an array of fruits", function (){
         var f = fruits('Apple,Orange,Mango');
--- Week-7/tdd-example/test/pinsone-exercise-7.2.js
+++ Week-7/tdd-example/test/pinsone-exercise-7.2.js
@@ -1,5 +1,15 @@
+/*
+============================================
+; Title: Pinson-Exercise 7.2.js
+; Author: Richard Krasso
+; Date: February 18th 2021
+; Modified By: James Pinson
+; Description: This assignment teaches us how to implement TDD.     
+;============================================
+*/
 var assert = require("assert");
 
+//This is our unit test. 
 describe("String#split", function(){
     it("should return an array of fruits", function(){
         assert(Array.isArray('Apple,Orange,Mango'.split(',')));
