653e4834c291e88246a688d7d81570ba18ea33c9
James <jpinson@my365.bellevue.edu>
2021-02-19 16:13:31 -0600

    Updated for week 7

--- ems/app.js
+++ ems/app.js
@@ -1,7 +1,23 @@
 var express = require("express");
 var http = require("http");
 var path = require("path");
+var mongoose = require("mongoose");
 var logger = require("morgan");
+
+var Employee = require("./models/employee");
+
+var mongoDB = "mongodb+srv://JamesPinson:Jimmyp369@buwebdev-cluster-1.iavdd.mongodb.net/test";
+
+mongoose.connect(mongoDB, {
+    useMongoClient:true
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function (){
+    console.log("Application connected to mLab MongoDB instance");
+});
 
 var app = express();
 
@@ -18,6 +34,13 @@ app.get("/", function (request, response){
 
     });
 });
+
+//model
+
+var employee = new Employee({
+    firstName: "James",
+    lastName: "Pinson"
+})
 
 http.createServer(app).listen(8080,function (){
     console.log("Application started on port 8080!");
